4e1cc900053776bfe2f632989cd42bcc850b5164
Francisco Barrento <francisco.barrento@gmail.com>
2024-05-03 22:28:21 +0100

    Second commit

--- src/Components/Button/Base.php
+++ src/Components/Button/Base.php
@@ -9,6 +9,9 @@ use Illuminate\View\View;
 
 class Base extends Component
 {
+
+    public string $sizeClasses = '';
+
     public function render(): View
     {
         return view('wordsphereui::button.base');
